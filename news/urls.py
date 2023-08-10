4cc4704588dbd6195f48f3272f4f8d9f775bbd7d
Alexp161 <116867471+Alexp161@users.noreply.github.com>
2023-08-10 03:35:37 +0300

    1

--- news/urls.py
+++ news/urls.py
@@ -1,13 +1,17 @@
 from django.urls import path
-from .views import NewsList, NewsDetail
+from .views import NewsList, NewsDetail, PostCreateView, PostUpdateView, PostDeleteView, search_news, create_article, ArticleListView
 
 app_name = 'news'
 
 urlpatterns = [
-path('', NewsListView.as_view(), name='news'),
-    path('<int:pk>/', NewsDetailView.as_view(), name='news_detail'),
-    path('add/', add_news, name='add_news'),
+    path('', NewsList.as_view(), name='news'),
+    path('<int:pk>/', NewsDetail.as_view(), name='news_detail'),
+    path('add/', PostCreateView.as_view(), name='add_news'),
     path('search/', search_news, name='search_results'),
-    path('<int:pk>/edit/', edit_news, name='edit_news'),
-    path('<int:pk>/delete/', delete_news, name='delete_news'),
+    path('<int:pk>/edit/', PostUpdateView.as_view(), name='edit_news'),
+    path('<int:pk>/delete/', PostDeleteView.as_view(), name='delete_news'),
+    path('create/', create_article, name='create_article'),
+    path('articles/', ArticleListView.as_view(), name='articles_list'),
 ]
+
+
--- news/views.py
+++ news/views.py
@@ -3,7 +3,8 @@ from .models import Post
 from .forms import PostForm
 from django.urls import reverse_lazy
 from django.db.models import Q
-
+from django.shortcuts import render, redirect
+from .forms import ArticleForm
 
 class NewsList(ListView):
     model = Post
@@ -12,35 +13,27 @@ class NewsList(ListView):
     ordering = ['-create_time']
     paginate_by = 5
 
-
 class NewsDetail(DetailView):
     model = Post
     template_name = 'newsobj.html'
     context_object_name = 'newsobj'
 
-
-class PostCreateView(FormView):
+class PostCreateView(CreateView):
+    model = Post
     template_name = 'add_post.html'
     form_class = PostForm
     success_url = reverse_lazy('news:news_list')
-
-    def form_valid(self, form):
-        form.save()
-        return super().form_valid(form)
-
 
 class PostUpdateView(UpdateView):
     model = Post
     template_name = 'edit_post.html'
     form_class = PostForm
-    success_url = reverse_lazy('news:news')
-
+    success_url = reverse_lazy('news:news_list')
 
 class PostDeleteView(DeleteView):
     model = Post
     template_name = 'delete_post.html'
     success_url = reverse_lazy('news:news_list')
-
 
 def search_news(request):
     title = request.GET.get('title', '')
@@ -57,3 +50,13 @@ def search_news(request):
 
     news_list = Post.objects.filter(search_q)
     return render(request, 'search_news.html', {'news_list': news_list})
+
+def create_article(request):
+    if request.method == "POST":
+        form = ArticleForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('name_of_the_articles_list_view')
+    else:
+        form = ArticleForm()
+    return render(request, 'news/create_article.html', {'form': form})
